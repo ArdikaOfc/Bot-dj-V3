087a2f6dcdaa60c7136c1b4fde880e48bef56ac8
Rf(Rifa) ArdikaOfc <uchihaardishu@gmail.com>
2026-03-14 20:21:13 +0700

    Update bot name and media references in menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -14,11 +14,11 @@ let handler = async (m, { conn }) => {
   let caption = `
 ${greeting}, *${name}* 
 
-Aku *Vestia Zeta*, bot WhatsApp yang siap membantu kamu ✨
+Aku *ᴹᴿ᭄༺DjBotzོ - MDོ ×፝֟͜×༻*, bot WhatsApp yang siap membantu kamu ✨
 
 ───〔 🤖 BOT INFO 〕───
-• *Nama* : Vestia Zeta
-• *Creator* : Allen
+• *Nama* : ᴹᴿ᭄༺DjBotzོ - MDོ ×፝֟͜×༻
+• *Creator* : ᴿꜰ᭄༺𝙰𝚛𝚍𝚒𝚔𝚊𝙾𝚏𝚌ོ ×፝֟͜×༻
 • *Versi* : 2.0.0
 • *System* : Plugins ESM
 ──────────────────
@@ -48,9 +48,9 @@ _Ketik salah satu menu di atas untuk melihat detail_
 `.trim()
 
   await conn.sendMessage(m.chat, {
-    image: fs.readFileSync('./media/zeta2.jpg'),
+    image: fs.readFileSync('./media/botdj2.jpg'),
     caption,
-    footer: 'Vestia Zeta - MD',
+    footer: 'ᴹᴿ᭄༺DjBotzོ - MDོ ×፝֟͜×༻',
     buttons: [
       {
         buttonId: '.allmenu',
@@ -62,9 +62,9 @@ _Ketik salah satu menu di atas untuk melihat detail_
     mentions: [m.sender],
     contextInfo: {
       externalAdReply: {
-        title: 'Vestia Zeta WhatsApp Bot',
+        title: 'ᴹᴿ᭄༺DjBotzོ - MDོ ×፝֟͜×༻ WhatsApp Bot',
         body: 'Simple • Fast • Multifungsi',
-        thumbnail: fs.readFileSync('./media/zeta1.jpg'),
+        thumbnail: fs.readFileSync('./media/botdj1.jpg'),
         sourceUrl: 'https://t.me/Allenkujou',
         mediaType: 1,
         renderLargerThumbnail: true
@@ -73,18 +73,11 @@ _Ketik salah satu menu di atas untuk melihat detail_
   }, { quoted: m })
 
   try {
-    await conn.sendFile(
-      m.chat,
-      'https://files.catbox.moe/7fje2p.mp3',
-      'menu.mp3',
-      null,
-      m,
-      true,
-      {
-        type: 'audioMessage',
-        ptt: true
-      }
-    )
+    await conn.sendMessage(m.chat, {
+    audio: { url: `https://raw.githubusercontent.com/Bell575/dat1/main/uploads/4c34d9-1761702925460.opus` },
+    mimetype: 'audio/mpeg',
+    ptt: true
+  }, { quoted: m })
   } catch (e) {
     console.error(e)
   }
