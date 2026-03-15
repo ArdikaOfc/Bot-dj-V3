3bad5289e6d7f9a49a8bfc38309c01240f01d097
Rf(Rifa) ArdikaOfc <uchihaardishu@gmail.com>
2026-03-15 23:15:29 +0700

    Add handler for 'coba' command with buttons

---
+++ plugins/_coba.js
@@ -0,0 +1,39 @@
+let handler = async (m, { conn }) => {
+
+
+				await conn.sendMessage(m.chat, {
+
+					text: 'Semoga Hoki😹',
+              footer: 'Powered by ᴹᴿ᭄༺DjBotzོ - MDོ ×፝֟͜×༻',
+
+					buttons: [{
+
+						buttonId: 'teshoki',
+
+						buttonText: { displayText: '\n' + pickRandom(anu)},
+
+						type: 1
+
+					},{
+
+						buttonId: 'cobacoba',
+
+						buttonText: { displayText: '\n' + pickRandom(anu)},
+
+						type: 1
+
+					}]
+
+				})
+
+			}
+
+handler.customPrefix = /^coba/i;
+handler.command = new RegExp();
+export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+const anu = ['Aku Monyet','Aku Kera','Aku Tolol','Aku Kaya','Aku Dewa','Aku Anjing','Aku Dongo','Aku Raja','Aku Sultan','Aku Baik','Aku Hitam','Aku Suki']
