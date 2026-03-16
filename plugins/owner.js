04cfb4111b914a3623e5f7c731d9a2953b15cd54
Rf(Rifa) ArdikaOfc <uchihaardishu@gmail.com>
2026-03-17 00:33:21 +0700

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -6,13 +6,13 @@ let name = await conn.getName(who)
 
   const sentMsg = await conn.sendContactArray(m.chat, [
     [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 Owner Bot `, `ɴᴏᴛ ғᴀᴍᴏᴜs ᴊᴜsᴛ ᴀʟᴏɴᴇ ʙᴏʏ`, `Dick`, `🇮🇩 Indonesia`, `📍 i don't know`, `👤 ᴏᴡɴᴇʀ ᴹᴿ᭄༺DjBotzོ - MDོ ×፝֟͜×༻`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ`, `📵 ᴅᴏɴᴛ sᴘᴀᴍ/ᴄᴀʟʟ ᴍᴇ 😢`, `ɴᴏᴛʜɪɴɢ`, `🇮🇩 Indonesia`, `📍 i don't know`, `ʜᴀɴʏᴀ ʙᴏᴛ ʙɪᴀsᴀ ʏᴀɴɢ ᴋᴀᴅᴀɴɢ sᴜᴋᴀ ᴇʀᴏʀ ☺`]
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ`, `📵 ᴅᴏɴᴛ sᴘᴀᴍ/ᴄᴀʟʟ ᴍᴇ 😢`, `ɴᴏᴛʜɪɴɢ`, `🇮🇩 Indonesia`, `📍 i don't know`, `ʜᴀɴʏᴀ ʙᴏᴛ ʙɪᴀsᴀ ʏᴀɴɢ ᴋᴀᴅᴀɴɢ sᴜᴋᴀ devoloper ☺`]
   ], fkontak)
   await m.reply(`ᴍʏ ᴏᴡɴᴇʀ ᴅᴏɴᴛ sᴘᴀᴍ ᴏʀ ʏᴏᴜ ᴡɪʟʟ ʙᴇ ʙʟᴏᴄᴋᴇᴅ`)
   } 
-handler.help = ['owner', 'creator']
+handler.help = ['devoloper', 'dev']
 handler.tags = ['info']
 
-handler.command = /^(owner|creator)$/i
+handler.command = /^(devoloper|dev)$/i
 
 export default handler
