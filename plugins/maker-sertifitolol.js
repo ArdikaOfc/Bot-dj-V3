a06ad63463f0ef3dedc48e7a89c06a726c8a4dcf
Rf(Rifa) ArdikaOfc <uchihaardishu@gmail.com>
2026-04-17 12:43:39 +0700

    Update maker-sertifitolol.js

--- plugins/maker-sertifitolol.js
+++ plugins/maker-sertifitolol.js
@@ -1,7 +1,7 @@
 /* 
 Sertifikat Tolol
 Plugin ESM 
-API : https://api.siputzx.my.id
+API : https://api.skylow.web.id/
 */
 import fetch from 'node-fetch'
 
@@ -9,7 +9,7 @@ let handler = async (m, { text, conn }) => {
   if (!text) return m.reply('⚠️ Masukkan nama untuk sertifikatnya!\n\nContoh:\n.sertiftolol Hilman')
 
   try {
-    let url = `https://api.siputzx.my.id/api/m/sertifikat-tolol?text=${encodeURIComponent(text)}`
+    let url = `https://api.skylow.web.id/api/maker/sertipikat?text=${encodeURIComponent(text)}`
     let res = await fetch(url)
     if (!res.ok) throw 'Gagal mengunduh gambar.'
 
