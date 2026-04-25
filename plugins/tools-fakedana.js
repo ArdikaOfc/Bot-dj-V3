d0a0ea829689d47733c97292e64e3302b515d6ca
Rf(Rifa) ArdikaOfc <uchihaardishu@gmail.com>
2026-04-26 05:46:58 +0700

    Create tools-fakedana.js

---
+++ plugins/tools-fakedana.js
@@ -0,0 +1,31 @@
+/*
+Wm: https://whatsapp.com/channel/0029VaF9C4zId7nOTFF8ZK0v
+Jgn hapus wm ku
+Fitur:  Fake dana
+Type : Plugins Esm 
+Api: https://api.skylow.web.id/
+*/
+let handler = async (m, { text, usedPrefix, command, conn }) => {
+    if (!text) return m.reply(`💬 Contoh penggunaan:\n${usedPrefix + command} 1000`);
+
+        try {
+            let number = text.replace(/[^0-9]/g, "");
+            if (!number) return m.reply(`Nominalnya berapa?\n💬 Contoh penggunaan:\n${usedPrefix + command} 10000`);
+            
+
+    m.reply('Tunggu Sedang Di proses...')
+
+    await conn.sendMessage(m.chat, { image: { url: `https://api.skylow.web.id/api/maker/fakedana?text=${text}` }, footer: global.footer, caption: `✨Sukses Membuat *FAKE DANA*`, contextInfo: { forwardingScore: 10, isForwarded: true, } }, { quoted: m })
+
+  } catch (e) {
+    m.reply(e.message)
+  }
+};
+
+handler.help = ['fakedana'];
+handler.command = ['fdana','fakedana'];
+handler.tags = ['tools'];
+handler.limit = 5
+handler.register = true
+
+export default handler;
