680b1e45a62d9557c36df441e3391e9e6b7e6915
Rf(Rifa) ArdikaOfc <uchihaardishu@gmail.com>
2026-04-26 09:25:56 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -1,11 +1,8 @@
-import fs from 'fs';
+import fs from 'fs'
 
-import fetch from 'node-fetch';
-
-import moment from 'moment-timezone';
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-await m.react('⏳')
+let handler = async (m, { conn }) => {
+  try {
+    await m.react('🕒')
 
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
 
@@ -27,18 +24,41 @@ let totalCommand = Object.values(global.plugins)
     let txt = `*乂  D J B O T Z  -  F E A T U R E*\n\n`;
 
     txt += `  • ᴄʀᴇᴀᴛᴏʀ : ᴿꜰ᭄༺ArdikaOfcོ ×፝֟͜×༻\n`;
+
     await m.react('✅')
 
-    await conn.relayMessage(m.chat,{
-pollResultSnapshotMessage:{
-pollVotes:[
-{optionName:`• 🗂️ 𝑻𝑶𝑻𝑨𝑳 𝑭𝑰𝑻𝑼𝑹: ${fitur.length}`,optionVoteCount:`${fitur.length}`},
-{optionName:`• 📄 𝑻𝑶𝑻𝑨𝑳 𝑪𝑶𝑴𝑴𝑨𝑵𝑫: ${totalCommand}`,optionVoteCount:`${totalCommand}`},
-],
-name:txt,
-pollType:0
-}
-},{})
+    const stats = [
+      `🔧 𝑻𝑶𝑻𝑨𝑳 𝑭𝑰𝑻𝑼𝑹 : ${fitur.length}`,
+      `📖 𝑻𝑶𝑻𝑨𝑳 𝑪𝑶𝑴𝑴𝑨𝑵𝑫 : ${totalCommand}`
+    ]
+
+    const content = {
+      pollResultSnapshotMessage: {
+        pollVotes: stats.map(v => ({
+          optionName: v,
+          optionVoteCount: Math.floor(Math.random() * 90000) + 10000
+        })),
+        name: txt,
+        contextInfo: {
+          forwardingScore: 127,
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+            newsletterJid: "120363199397739684@newsletter",
+            serverMessageId: 0,
+            newsletterName: "ᴹᴿ᭄༺DjBotzོ - MDོ ×፝֟͜×༻"
+          },
+          forwardOrigin: 0
+        },
+        pollType: 0
+      }
+    }
+
+    await conn.relayMessage(m.chat, content, {})
+
+  } catch (e) {
+    console.error(e)
+    m.reply('Terjadi error.')
+  }
 }
 
 handler.help = ['totalfitur'];
