4947101c8e3d5ee6442452d11d52e181ddba3589
Rf(Rifa) ArdikaOfc <uchihaardishu@gmail.com>
2026-04-29 13:40:00 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -126,7 +126,7 @@ export async function handler(chatUpdate) {
                 if (!('antiLinkdelete' in chat)) chat.antiLinkdelete = false
                 if (!('antiLinkWa' in chat)) chat.antiLinkwa = false 
                 if (!('menu' in chat)) chat.menu = false
-                if (!('lastcleartmp' in chat)) chat.lastcleartmp = true
+                if (!('autocleartmp' in chat)) chat.autocleartmp = true
                 if (!('onlyadmin' in chat)) chat.onlyadmin = false
                 if (!isNumber(chat.expired)) chat.expired = 0
                 if (!('listrespon' in chat)) chat.listrespon = {}
@@ -155,7 +155,7 @@ export async function handler(chatUpdate) {
                     antiLinkdelete: false,
                     antiLinkWa: false,
                     menu: true,
-                    lastcleartmp true
+                    autocleartmp true
                     onlyadmin: false,
                     listrespon: {},
                     produk: {}
