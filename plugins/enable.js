2fb20c9898997199ea195f18019d9aaf55f53c68
Rf(Rifa) ArdikaOfc <uchihaardishu@gmail.com>
2026-05-03 10:13:18 +0700

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -87,7 +87,7 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
                 global.dfail('owner', m, conn)
                 throw false
             }
-            bot.cleartmp = isEnable
+            setting.cleartmp = isEnable
             break
 
     case 'owneronly':
